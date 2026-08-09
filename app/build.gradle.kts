3b09eecf78c9cf59009c933923e5fb92827fa22f
koreaone10-del <koreaone10@gmail.com>
2026-08-09 22:51:42 +0100

    Delete app directory

--- app/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    // مكتبة VirtualApp (يجب بنائها من المصدر أو استخدام نسخة معدلة)
-    implementation(files("libs/virtualapp.aar"))
-    
-    // أدوات الخطاف: استخدام Xposed-like أو انعكاس عادي
-    // سنستخدم الانعكاس وتعديل الكائنات يدوياً (بدون روت)
-    implementation("com.google.code.gson:gson:2.10.1") // لبعض السيريالايز
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-}
